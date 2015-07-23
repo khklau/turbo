064a22dd6872a213af48f52675ad2c41da55d4c2
Kean H Lau <khklau@users.noreply.github.com>
2015-07-24 09:22:10 +1000

    adding an alternate implementation based on pairs of pipe ends

--- src/ipc/posix/pipe.cxx
+++ src/ipc/posix/pipe.cxx
@@ -4,9 +4,166 @@
 #include <cassert>
 #include <cerrno>
 
+namespace {
+
+using namespace turbo::ipc::posix;
+
+replace_result replace(int handle, int stdstream, int options)
+{
+    replace_result tmp = replace_result::success;
+    if (handle >= 0 && dup3(handle, stdstream, options) == -1)
+    {
+	switch (errno)
+	{
+	    case EINTR:
+	    {
+		tmp = replace_result::interrupted;
+		break;
+	    }
+	    case EBUSY:
+	    {
+		throw race_condition_error();
+	    }
+	    case EMFILE:
+	    {
+		throw process_limit_reached_error();
+	    }
+	    case EBADF:
+	    case EINVAL:
+	    {
+		assert(false);
+	    }
+	}
+    }
+    return tmp;
+}
+
+} // anonymous namespace
+
 namespace turbo {
 namespace ipc {
 namespace posix {
+
+pipe_front::pipe_front(int options, const handle& handle) :
+	options_(options),
+	handle_(handle)
+{ }
+
+pipe_front::pipe_front(pipe_front&& other) noexcept :
+	options_(other.options_),
+	handle_(other.handle_)
+{
+    other.options_ = 0;
+    other.handle_ = -1;
+}
+
+pipe_front::~pipe_front()
+{
+    if (handle_ >= 0)
+    {
+	close(handle_);
+    }
+}
+
+pipe_front& pipe_front::operator=(pipe_front&& other)
+{
+    if (this != &other)
+    {
+	options_ = other.options_;
+	handle_ = other.handle_;
+	other.options_ = 0;
+	other.handle_ = -1;
+    }
+    return *this;
+}
+
+replace_result pipe_front::replace_stdin()
+{
+    return ::replace(handle_, STDIN_FILENO, options_);
+}
+
+pipe_back::pipe_back(int options, const handle& handle) :
+	options_(options),
+	handle_(handle)
+{ }
+
+pipe_back::pipe_back(pipe_back&& other) noexcept :
+	options_(other.options_),
+	handle_(other.handle_)
+{
+    other.options_ = 0;
+    other.handle_ = -1;
+}
+
+pipe_back::~pipe_back()
+{
+    if (handle_ >= 0)
+    {
+	close(handle_);
+    }
+}
+
+pipe_back& pipe_back::operator=(pipe_back&& other)
+{
+    if (this != &other)
+    {
+	options_ = other.options_;
+	handle_ = other.handle_;
+	other.options_ = 0;
+	other.handle_ = -1;
+    }
+    return *this;
+}
+
+replace_result pipe_back::replace_stdout()
+{
+    return ::replace(handle_, STDOUT_FILENO, options_);
+}
+
+replace_result pipe_back::replace_stderr()
+{
+    return ::replace(handle_, STDERR_FILENO, options_);
+}
+
+pipe2 make_pipe(std::vector<pipe_option>& options)
+{
+    int opt = 0;
+    for (auto iter = options.cbegin(); iter != options.cend(); ++iter)
+    {
+	switch (*iter)
+	{
+	    case pipe_option::non_blocking:
+	    {
+		opt |= O_NONBLOCK;
+		break;
+	    }
+	    case pipe_option::fork_compatible:
+	    {
+		opt |= O_CLOEXEC;
+		break;
+	    }
+	}
+    }
+    int tmp[2];
+    int result = ::pipe2(tmp, opt);
+    switch (result)
+    {
+	case EMFILE:
+	{
+	    throw process_limit_reached_error();
+	}
+	case ENFILE:
+	{
+	    throw system_limit_reached_error();
+	}
+	case EFAULT:
+	case EINVAL:
+	{
+	    assert(false);
+	}
+    }
+    return std::make_pair(pipe_front(opt, tmp[0]), pipe_back(opt, tmp[1]));
+}
 
 pipe::pipe(std::vector<option>& options) :
 	pipe(init(options))
@@ -32,7 +189,7 @@ std::tuple<int, pipe::handle, pipe::handle> pipe::init(std::vector<pipe::option>
 	}
     }
     int tmp[2];
-    int result = pipe2(tmp, opt);
+    int result = ::pipe2(tmp, opt);
     switch (result)
     {
 	case EMFILE:
--- src/ipc/posix/pipe.hpp
+++ src/ipc/posix/pipe.hpp
@@ -2,11 +2,70 @@
 #define TURBO_IPC_POSIX_PIPE_HPP
 
 #include <tuple>
+#include <utility>
 #include <vector>
 
 namespace turbo {
 namespace ipc {
 namespace posix {
+
+enum class pipe_option
+{
+    non_blocking,
+    fork_compatible
+};
+
+enum class replace_result
+{
+    success,
+    interrupted
+};
+
+class pipe_front;
+class pipe_back;
+typedef std::pair<pipe_front, pipe_back> pipe2;
+struct process_limit_reached_error {};
+struct system_limit_reached_error {};
+struct race_condition_error {};
+
+pipe2 make_pipe(std::vector<pipe_option>& options);
+
+class pipe_front
+{
+public:
+    pipe_front(pipe_front&& other) noexcept;
+    ~pipe_front();
+    pipe_front& operator=(pipe_front&& other);
+    replace_result replace_stdin();
+private:
+    typedef int handle;
+    friend pipe2 make_pipe(std::vector<pipe_option>& options);
+    pipe_front() = delete;
+    pipe_front(int options, const handle& handle);
+    pipe_front(const pipe_front& other) = delete;
+    pipe_front& operator=(const pipe_front& other) = delete;
+    int options_;
+    handle handle_;
+};
+
+class pipe_back
+{
+public:
+    pipe_back(pipe_back&& other) noexcept;
+    ~pipe_back();
+    pipe_back& operator=(pipe_back&& other);
+    replace_result replace_stdout();
+    replace_result replace_stderr();
+private:
+    typedef int handle;
+    friend pipe2 make_pipe(std::vector<pipe_option>& options);
+    pipe_back() = delete;
+    pipe_back(int options, const handle& handle);
+    pipe_back(const pipe_back& other) = delete;
+    pipe_back& operator=(const pipe_back& other) = delete;
+    int options_;
+    handle handle_;
+};
 
 class pipe
 {
