21a72595ebf3ec0efb50ba9d6443d5311371e841
Kean H Lau <khklau@users.noreply.github.com>
2015-08-04 09:36:52 +1000

    adding the toolset attribute C preprocessor definitions

---
+++ src/toolset/attribute.hpp
@@ -0,0 +1,29 @@
+#ifndef TURBO_TOOLSET_ATTRIBUTE_HPP
+#define TURBO_TOOLSET_ATTRIBUTE_HPP
+
+#if defined( _WIN32)
+#if defined(_MSC_VER)
+#define TURBO_DECL_EXPORT __declspec(dllexport)
+#define TURBO_DECL_IMPORT __declspec(dllimport)
+#endif
+#else
+#if defined(__GNUC__) || defined(__clang__)
+#define TURBO_DECL_EXPORT __attribute__((visibility("default")))
+#define TURBO_DECL_IMPORT
+#endif
+#endif
+
+#if !defined(TURBO_DECL_EXPORT)
+#define TURBO_DECL_EXPORT
+#endif
+#if !defined(TURBO_DECL_IMPORT)
+#define TURBO_DECL_IMPORT
+#endif
+
+#if defined(SHLIB_BUILD)
+#define TURBO_SYMBOL_DECL TURBO_DECL_EXPORT
+#else
+#define TURBO_SYMBOL_DECL TURBO_DECL_IMPORT
+#endif
+
+#endif
