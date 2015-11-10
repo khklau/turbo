9fbd4369c09d9a420b2267bea794e71c21083d1e
Kean H Lau <khklau@users.noreply.github.com>
2015-11-10 17:58:32 +1100

    adding in initial implementation for a lock free single producer, single consumer ring queue

---
+++ src/container/spsc_ring_queue.hpp
@@ -0,0 +1,85 @@
+#ifndef TURBO_CONTAINER_SPSC_RING_QUEUE_HPP
+#define TURBO_CONTAINER_SPSC_RING_QUEUE_HPP
+
+#include <cstdint>
+#include <array>
+#include <atomic>
+#include <functional>
+#include <vector>
+#include <turbo/toolset/attribute.hpp>
+
+namespace turbo {
+namespace container {
+
+template <class value_t, class allocator_t = std::allocator<value_t>> class spsc_key;
+
+template <class value_t, class allocator_t = std::allocator<value_t>>
+class spsc_producer
+{
+public:
+    typedef value_t value_type;
+    typedef allocator_t allocator_type;
+    typedef spsc_key<value_t, allocator_t> key;
+    enum class result
+    {
+	success,
+	queue_full
+    };
+    spsc_producer(const key&,
+		    std::vector<value_t, allocator_t>& buffer,
+		    std::atomic<uint32_t>& head,
+		    std::atomic<uint32_t>& tail);
+    result try_enqueue(const value_t& input);
+private:
+    std::vector<value_t, allocator_t>& buffer_;
+    std::atomic<uint32_t>& head_;
+    std::atomic<uint32_t>& tail_;
+};
+
+template <class value_t, class allocator_t = std::allocator<value_t>>
+class spsc_consumer
+{
+public:
+    typedef value_t value_type;
+    typedef allocator_t allocator_type;
+    typedef spsc_key<value_t, allocator_t> key;
+    spsc_consumer(const key&,
+		    std::vector<value_t, allocator_t>& buffer,
+		    std::atomic<uint32_t>& head,
+		    std::atomic<uint32_t>& tail);
+    enum class result
+    {
+	success,
+	queue_empty
+    };
+    result try_dequeue(value_t& output);
+private:
+    std::vector<value_t, allocator_t>& buffer_;
+    std::atomic<uint32_t>& head_;
+    std::atomic<uint32_t>& tail_;
+};
+
+template <class value_t, class allocator_t = std::allocator<value_t>>
+class spsc_ring_queue
+{
+public:
+    typedef value_t value_type;
+    typedef allocator_t allocator_type;
+    typedef spsc_producer<value_t, allocator_t> producer;
+    typedef spsc_consumer<value_t, allocator_t> consumer;
+    spsc_ring_queue(uint32_t capacity);
+    producer& get_producer() { return producer_; }
+    consumer& get_consumer() { return consumer_; }
+private:
+    typedef std::vector<value_t, allocator_t> vector_type;
+    std::vector<value_t, allocator_t> buffer_;
+    std::atomic<uint32_t> head_;
+    std::atomic<uint32_t> tail_;
+    producer producer_;
+    consumer consumer_;
+};
+
+} // namespace container
+} // namespace turbo
+
+#endif
---
+++ src/container/spsc_ring_queue.hxx
@@ -0,0 +1,90 @@
+#ifndef TURBO_CONTAINER_SPSC_RING_QUEUE_HXX
+#define TURBO_CONTAINER_SPSC_RING_QUEUE_HXX
+
+#include <turbo/container/spsc_ring_queue.hpp>
+#include <limits>
+
+namespace turbo {
+namespace container {
+
+template <class value_t, class allocator_t>
+class spsc_key
+{
+    spsc_key() { }
+    friend class spsc_ring_queue<value_t, allocator_t>;
+};
+
+template <class value_t, class allocator_t>
+spsc_producer<value_t, allocator_t>::spsc_producer(
+		const key&,
+		std::vector<value_t, allocator_t>& buffer,
+		std::atomic<uint32_t>& head,
+		std::atomic<uint32_t>& tail) :
+	buffer_(buffer),
+	head_(head),
+	tail_(tail)
+{ }
+
+template <class value_t, class allocator_t>
+typename spsc_producer<value_t, allocator_t>::result spsc_producer<value_t, allocator_t>::try_enqueue(const value_t& input)
+{
+    uint32_t head = head_.load(std::memory_order_consume);
+    uint32_t tail = tail_.load(std::memory_order_consume);
+    if (head < tail)
+    {
+	// overflow has occurred
+	if (((head - 0 + 1) + (std::numeric_limits<uint32_t>::max() - tail + 1)) == buffer_.capacity())
+	{
+	    return result::queue_full;
+	}
+    }
+    else
+    {
+	if (head - tail == buffer_.capacity())
+	{
+	    return result::queue_full;
+	}
+    }
+    buffer_[head] = input;
+    head_.fetch_add(1, std::memory_order_seq_cst);
+    return result::success;
+}
+
+template <class value_t, class allocator_t>
+spsc_consumer<value_t, allocator_t>::spsc_consumer(
+		const key&,
+		std::vector<value_t, allocator_t>& buffer,
+		std::atomic<uint32_t>& head,
+		std::atomic<uint32_t>& tail) :
+	buffer_(buffer),
+	head_(head),
+	tail_(tail)
+{ }
+
+template <class value_t, class allocator_t>
+typename spsc_consumer<value_t, allocator_t>::result spsc_consumer<value_t, allocator_t>::try_dequeue(value_t& output)
+{
+    uint32_t head = head_.load(std::memory_order_consume);
+    uint32_t tail = tail_.load(std::memory_order_consume);
+    if (head == tail)
+    {
+	return result::queue_full;
+    }
+    output = buffer_[tail];
+    tail_.fetch_add(1, std::memory_order_seq_cst);
+    return result::success;
+}
+
+template <class value_t, class allocator_t>
+spsc_ring_queue<value_t, allocator_t>::spsc_ring_queue(uint32_t capacity) :
+	buffer_(capacity),
+	head_(0),
+	tail_(0),
+	producer_(typename producer::key(), buffer_, head_, tail_),
+	consumer_(typename consumer::key(), buffer_, head_, tail_)
+{ }
+
+} // namespace container
+} // namespace turbo
+
+#endif
