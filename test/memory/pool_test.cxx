7bae78ece184f41d93ee755791beac9cd95f9050
Kean H Lau <khklau@users.noreply.github.com>
2016-07-14 18:35:24 +1000

    added more test cases

--- test/memory/pool_test.cxx
+++ test/memory/pool_test.cxx
@@ -28,3 +28,49 @@ TEST(pool_test, make_unique_basic)
 	EXPECT_EQ(std::string("lmn456"), *result3.second) << "Unique pool string didn't initialise";
     }
 }
+
+TEST(pool_test, make_unique_full)
+{
+    typedef tme::block_pool<sizeof(std::string)> string_pool;
+    string_pool pool1(2U, 1U);
+    {
+	auto result1 = pool1.make_unique<std::string>("abc123");
+	EXPECT_EQ(tme::make_result::success, result1.first) << "Make unique pool string failed";
+	EXPECT_EQ(std::string("abc123"), *result1.second) << "Unique pool string didn't initialise";
+	auto result2 = pool1.make_unique<std::string>("xyz789");
+	EXPECT_EQ(tme::make_result::success, result2.first) << "Make unique pool string failed";
+	EXPECT_EQ(std::string("xyz789"), *result2.second) << "Unique pool string didn't initialise";
+	auto result3 = pool1.make_unique<std::string>("lmn456");
+	EXPECT_EQ(tme::make_result::pool_full, result3.first) << "Full pool is still allocating";
+    }
+}
+
+TEST(pool_test, make_unique_recycle)
+{
+    typedef tme::block_pool<sizeof(std::string)> string_pool;
+    string_pool pool1(3U, 1U);
+    {
+	auto result1 = pool1.make_unique<std::string>("abc123");
+	EXPECT_EQ(tme::make_result::success, result1.first) << "Make unique pool string failed";
+	EXPECT_EQ(std::string("abc123"), *result1.second) << "Unique pool string didn't initialise";
+	auto result2 = pool1.make_unique<std::string>("xyz789");
+	EXPECT_EQ(tme::make_result::success, result2.first) << "Make unique pool string failed";
+	EXPECT_EQ(std::string("xyz789"), *result2.second) << "Unique pool string didn't initialise";
+	auto result3 = pool1.make_unique<std::string>("lmn456");
+	EXPECT_EQ(tme::make_result::success, result3.first) << "Make unique pool string failed";
+	EXPECT_EQ(std::string("lmn456"), *result3.second) << "Unique pool string didn't initialise";
+    }
+    {
+	auto result4 = pool1.make_unique<std::string>("foo");
+	EXPECT_EQ(tme::make_result::success, result4.first) << "Make reycled unique pool string failed";
+	EXPECT_EQ(std::string("foo"), *result4.second) << "Recycled unique pool string didn't initialise";
+	auto result5 = pool1.make_unique<std::string>("bar");
+	EXPECT_EQ(tme::make_result::success, result5.first) << "Make reycled unique pool string failed";
+	EXPECT_EQ(std::string("bar"), *result5.second) << "Recycled unique pool string didn't initialise";
+	auto result6 = pool1.make_unique<std::string>("blah");
+	EXPECT_EQ(tme::make_result::success, result6.first) << "Make reycled unique pool string failed";
+	EXPECT_EQ(std::string("blah"), *result6.second) << "Recycled unique pool string didn't initialise";
+	auto result7 = pool1.make_unique<std::string>("foobar");
+	EXPECT_EQ(tme::make_result::pool_full, result7.first) << "Full pool is still allocating";
+    }
+}
