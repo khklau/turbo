5b95a9d1a8093639c9cd3307df8990ae3cc55320
Kean H Lau <khklau@users.noreply.github.com>
2016-07-21 09:11:11 +1000

    adding more shared/unique mixed test cases

--- test/memory/pool_test.cxx
+++ test/memory/pool_test.cxx
@@ -403,7 +403,7 @@ TEST(pool_test, make_shared_array)
     EXPECT_EQ(nullptr, result6.second.get()) << "Pointer returned from full pool is not null";
 }
 
-TEST(pool_test, make_mixed_basic)
+TEST(pool_test, make_mixed_full)
 {
     typedef tme::block_pool<sizeof(std::string)> string_pool;
     string_pool pool1(4U, 1U);
@@ -420,5 +420,105 @@ TEST(pool_test, make_mixed_basic)
 	auto result4 = pool1.make_shared<std::string>("$%^");
 	EXPECT_EQ(tme::make_result::success, result4.first) << "Make shared pool string failed";
 	EXPECT_EQ(std::string("$%^"), *result4.second) << "Shared pool string didn't initialise";
+	auto result5 = pool1.make_shared<std::string>("lmn456");
+	EXPECT_EQ(tme::make_result::pool_full, result5.first) << "Full pool is still allocating";
+	EXPECT_EQ(nullptr, result5.second.get()) << "Pointer returned from full pool is not null";
     }
+}
+
+TEST(pool_test, make_mixed_transferred)
+{
+    typedef tme::block_pool<sizeof(std::string)> string_pool;
+    string_pool pool1(4U, 1U);
+    {
+	tme::pool_unique_ptr<std::string> move1;
+	std::shared_ptr<std::string> copy2;
+	tme::pool_unique_ptr<std::string> move3;
+	std::shared_ptr<std::string> copy4;
+	{
+	    auto result1 = pool1.make_unique<std::string>("abc123");
+	    EXPECT_EQ(tme::make_result::success, result1.first) << "Make unique pool string failed";
+	    EXPECT_EQ(std::string("abc123"), *result1.second) << "Unique pool string didn't initialise";
+	    auto result2 = pool1.make_shared<std::string>("!@#");
+	    EXPECT_EQ(tme::make_result::success, result2.first) << "Make shared pool string failed";
+	    EXPECT_EQ(std::string("!@#"), *result2.second) << "Shared pool string didn't initialise";
+	    auto result3 = pool1.make_unique<std::string>("xyz789");
+	    EXPECT_EQ(tme::make_result::success, result3.first) << "Make unique pool string failed";
+	    EXPECT_EQ(std::string("xyz789"), *result3.second) << "Unique pool string didn't initialise";
+	    auto result4 = pool1.make_shared<std::string>("$%^");
+	    EXPECT_EQ(tme::make_result::success, result4.first) << "Make shared pool string failed";
+	    EXPECT_EQ(std::string("$%^"), *result4.second) << "Shared pool string didn't initialise";
+	    move1 = std::move(result1.second);
+	    copy2 = result2.second;
+	    move3 = std::move(result3.second);
+	    copy4 = result4.second;
+	}
+	auto result5 = pool1.make_shared<std::string>("lmn456");
+	EXPECT_EQ(tme::make_result::pool_full, result5.first) << "Full pool is still allocating";
+	EXPECT_EQ(nullptr, result5.second.get()) << "Pointer returned from full pool is not null";
+    }
+    auto result6 = pool1.make_unique<std::string>("xyz789");
+    EXPECT_EQ(tme::make_result::success, result6.first) << "Make unique pool string failed";
+    EXPECT_EQ(std::string("xyz789"), *result6.second) << "Unique pool string didn't initialise";
+    auto result7 = pool1.make_shared<std::string>("$%^");
+    EXPECT_EQ(tme::make_result::success, result7.first) << "Make shared pool string failed";
+    EXPECT_EQ(std::string("$%^"), *result7.second) << "Shared pool string didn't initialise";
+}
+
+TEST(pool_test, make_mixed_array)
+{
+    typedef std::array<std::uint32_t, 8> uint_array;
+    typedef tme::block_pool<sizeof(uint_array)> array_pool;
+    array_pool pool1(2U, 1U);
+    {
+	std::shared_ptr<uint_array> copy1;
+	tme::pool_unique_ptr<uint_array> move2;
+	{
+	    auto result1 = pool1.make_shared<uint_array>();
+	    EXPECT_EQ(tme::make_result::success, result1.first) << "Make shared pool array failed";
+	    std::fill(result1.second->begin(), result1.second->end(), 15U);
+	    auto result2 = pool1.make_unique<uint_array>();
+	    EXPECT_EQ(tme::make_result::success, result2.first) << "Make unique pool array failed";
+	    std::fill(result2.second->begin(), result2.second->end(), 256U);
+	    copy1 = result1.second;
+	    move2 = std::move(result2.second);
+	}
+	for (std::uint32_t& value: *copy1)
+	{
+	    value *= 2;
+	}
+	for (std::uint32_t& value: *move2)
+	{
+	    value *= 2;
+	}
+	for (const std::uint32_t& value: *copy1)
+	{
+	    EXPECT_EQ(15U * 2, value) << "Shared pool array didn't initialise correctly at index " << &value - &(*(copy1->cbegin()));
+	}
+	for (const std::uint32_t& value: *move2)
+	{
+	    EXPECT_EQ(256U * 2, value) << "Unique pool array didn't initialise correctly at index " << &value - &(*(move2->cbegin()));
+	}
+	auto result3 = pool1.make_unique<uint_array>();
+	EXPECT_EQ(tme::make_result::pool_full, result3.first) << "Full pool is still allocating";
+	EXPECT_EQ(nullptr, result3.second.get()) << "Pointer returned from full pool is not null";
+    }
+    auto result4 = pool1.make_shared<uint_array>();
+    EXPECT_EQ(tme::make_result::success, result4.first) << "Make recyced shared pool array failed";
+    std::fill(result4.second->begin(), result4.second->end(), 256U);
+    std::shared_ptr<uint_array> copy3 = result4.second;
+    auto result5 = pool1.make_unique<uint_array>();
+    EXPECT_EQ(tme::make_result::success, result5.first) << "Make reycled unique pool array failed";
+    std::fill(result5.second->begin(), result5.second->end(), 15U);
+    for (const std::uint32_t& value: *(result4.second))
+    {
+	EXPECT_EQ(256U, value) << "Shared pool array didn't initialise correctly at index " << &value - &(*(result4.second->cbegin()));
+    }
+    for (const std::uint32_t& value: *(result5.second))
+    {
+	EXPECT_EQ(15U, value) << "Unique pool array didn't initialise correctly at index " << &value - &(*(result5.second->cbegin()));
+    }
+    auto result6 = pool1.make_shared<uint_array>();
+    EXPECT_EQ(tme::make_result::pool_full, result6.first) << "Full pool is still allocating";
+    EXPECT_EQ(nullptr, result6.second.get()) << "Pointer returned from full pool is not null";
 }
