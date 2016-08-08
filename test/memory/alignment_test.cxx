a7d7b64b0379819d861fc3294b51ca5f6b3a5e78
Kean H Lau <khklau@users.noreply.github.com>
2016-08-08 18:31:49 +1000

    added more test cases

--- test/memory/alignment_test.cxx
+++ test/memory/alignment_test.cxx
@@ -6,16 +6,51 @@ namespace tme = turbo::memory;
 
 TEST(alignment_test, align_single_element)
 {
-    std::uint8_t buffer[64];
-    std::uint8_t* pointer1 = &buffer[0];
-    if ((reinterpret_cast<std::uintptr_t>(pointer1) % 2U) == 0)
+    std::uint8_t buffer1[64];
+    std::uint8_t* actual_ptr1 = &buffer1[0];
+    std::size_t actual_size1 = sizeof(buffer1);
+    if ((reinterpret_cast<std::uintptr_t>(actual_ptr1) % 2U) == 0)
     {
-	++pointer1;
+	++actual_ptr1;
     }
-    std::uint8_t* expected1 = pointer1 + 1;
-    std::size_t size1 = sizeof(buffer);
-    void* tmp1 = static_cast<void*>(pointer1);
-    tme::align(sizeof(std::uint16_t), sizeof(std::uint16_t), tmp1, size1);
-    pointer1 = static_cast<std::uint8_t*>(tmp1);
-    EXPECT_EQ(expected1, pointer1) << "Pointer is not aligned";
+    std::uint8_t* expected_ptr1 = actual_ptr1 + 1;
+    std::size_t expected_size1 = actual_size1 - 1;
+    void* tmp1 = static_cast<void*>(actual_ptr1);
+    tme::align(sizeof(std::uint16_t), sizeof(std::uint16_t), tmp1, actual_size1);
+    actual_ptr1 = static_cast<std::uint8_t*>(tmp1);
+    EXPECT_EQ(expected_ptr1, actual_ptr1) << "Pointer is not aligned";
+    EXPECT_EQ(expected_size1, actual_size1) << "Incorrect available space";
+
+    std::uint8_t buffer2[64];
+    std::uint8_t* actual_ptr2 = &buffer2[0];
+    std::size_t actual_size2 = sizeof(buffer2);
+    if ((reinterpret_cast<std::uintptr_t>(actual_ptr2) % 2U) == 0)
+    {
+	++actual_ptr2;
+    }
+    std::uint8_t* expected_ptr2 = actual_ptr2 + 7;
+    std::size_t expected_size2 = actual_size2 - 7;
+    void* tmp2 = static_cast<void*>(actual_ptr2);
+    tme::align(sizeof(std::uint64_t), sizeof(std::uint64_t), tmp2, actual_size2);
+    actual_ptr2 = static_cast<std::uint8_t*>(tmp2);
+    EXPECT_EQ(expected_ptr2, actual_ptr2) << "Pointer is not aligned";
+    EXPECT_EQ(expected_size2, actual_size2) << "Incorrect available space";
+}
+
+TEST(alignment_test, align_multiple_element)
+{
+    std::uint8_t buffer1[64];
+    std::uint8_t* actual_ptr1 = &buffer1[0];
+    std::size_t actual_size1 = sizeof(buffer1);
+    if ((reinterpret_cast<std::uintptr_t>(actual_ptr1) % 2U) == 0)
+    {
+	++actual_ptr1;
+    }
+    std::uint8_t* expected_ptr1 = actual_ptr1 + 3;
+    std::size_t expected_size1 = actual_size1 - 3;
+    void* tmp1 = static_cast<void*>(actual_ptr1);
+    tme::align(sizeof(std::uint32_t), sizeof(std::uint16_t), tmp1, actual_size1);
+    actual_ptr1 = static_cast<std::uint8_t*>(tmp1);
+    EXPECT_EQ(expected_ptr1, actual_ptr1) << "Pointer is not aligned";
+    EXPECT_EQ(expected_size1, actual_size1) << "Incorrect available space";
 }
