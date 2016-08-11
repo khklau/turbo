ae39ef472c723ccf656fdb072bc6e660995a8e5c
Kean H Lau <khklau@users.noreply.github.com>
2016-08-12 09:30:55 +1000

    adding in test cases for more realistic value types

--- test/memory/block_test.cxx
+++ test/memory/block_test.cxx
@@ -35,7 +35,7 @@ TEST(block_test, allocate_basic)
 
 TEST(block_test, free_basic)
 {
-    tme::block block1(sizeof(std::uint64_t), 3U, alignof(std::uint64_t));
+    tme::block block1(sizeof(std::uint16_t), 3U, alignof(std::uint64_t));
     void* result1 = block1.allocate();
     void* result2 = block1.allocate();
     void* result3 = block1.allocate();
@@ -46,7 +46,90 @@ TEST(block_test, free_basic)
 
 TEST(block_test, recycle_basic)
 {
-    tme::block block1(sizeof(std::uint64_t), 3U, alignof(std::uint64_t));
+    tme::block block1(sizeof(std::uint32_t), 3U, alignof(std::uint64_t));
+    void* result1 = block1.allocate();
+    void* result2 = block1.allocate();
+    EXPECT_NE(nullptr, result1) << "Allocation failed";
+    EXPECT_NE(nullptr, result2) << "Allocation failed";
+    ASSERT_NO_THROW(block1.free(result1)) << "Free failed";
+    void* result3 = block1.allocate();
+    void* result4 = block1.allocate();
+    EXPECT_EQ(nullptr, block1.allocate()) << "Full block still allocated";
+    ASSERT_NO_THROW(block1.free(result2)) << "Free failed";
+    ASSERT_NO_THROW(block1.free(result3)) << "Free failed";
+    ASSERT_NO_THROW(block1.free(result4)) << "Free failed";
+    EXPECT_NE(nullptr, block1.allocate()) << "Allocation failed";
+    EXPECT_NE(nullptr, block1.allocate()) << "Allocation failed";
+    EXPECT_NE(nullptr, block1.allocate()) << "Allocation failed";
+    EXPECT_EQ(nullptr, block1.allocate()) << "Full block still allocated";
+}
+
+struct record
+{
+    record();
+    record(uint16_t f, uint32_t s, uint64_t t, void* o);
+    record(const record& other);
+    bool operator==(const record& other) const;
+    uint16_t first;
+    uint32_t second;
+    uint64_t third;
+    void* fourth;
+};
+
+record::record()
+    :
+	first(0U),
+	second(0U),
+	third(0U),
+	fourth(nullptr)
+{ }
+
+record::record(uint16_t f, uint32_t s, uint64_t t, void* o)
+    :
+	first(f),
+	second(s),
+	third(t),
+	fourth(o)
+{ }
+
+record::record(const record& other)
+    :
+	first(other.first),
+	second(other.second),
+	third(other.third),
+	fourth(other.fourth)
+{ }
+
+bool record::operator==(const record& other) const
+{
+    return first == other.first && second == other.second && third == other.third && fourth == other.fourth;
+}
+
+TEST(block_test, recycle_struct)
+{
+    tme::block block1(sizeof(record), 3U, alignof(record));
+    void* result1 = block1.allocate();
+    void* result2 = block1.allocate();
+    EXPECT_NE(nullptr, result1) << "Allocation failed";
+    EXPECT_NE(nullptr, result2) << "Allocation failed";
+    ASSERT_NO_THROW(block1.free(result1)) << "Free failed";
+    void* result3 = block1.allocate();
+    void* result4 = block1.allocate();
+    EXPECT_EQ(nullptr, block1.allocate()) << "Full block still allocated";
+    ASSERT_NO_THROW(block1.free(result2)) << "Free failed";
+    ASSERT_NO_THROW(block1.free(result3)) << "Free failed";
+    ASSERT_NO_THROW(block1.free(result4)) << "Free failed";
+    EXPECT_NE(nullptr, block1.allocate()) << "Allocation failed";
+    EXPECT_NE(nullptr, block1.allocate()) << "Allocation failed";
+    EXPECT_NE(nullptr, block1.allocate()) << "Allocation failed";
+    EXPECT_EQ(nullptr, block1.allocate()) << "Full block still allocated";
+}
+
+typedef std::array<char, 64> char_64_array;
+
+TEST(block_test, recycle_array)
+{
+    tme::block block1(sizeof(char_64_array), 3U, alignof(char_64_array));
     void* result1 = block1.allocate();
     void* result2 = block1.allocate();
     EXPECT_NE(nullptr, result1) << "Allocation failed";
