fba7b87ad4329456382aaed0289ed6c44f84578b
Kean H Lau <khklau@users.noreply.github.com>
2016-09-05 09:42:05 +1000

    ceiling_bound now handles invalid multiplier arguments

--- src/math/summation.cxx
+++ src/math/summation.cxx
@@ -3,14 +3,18 @@
 namespace turbo {
 namespace math {
 
-std::uint64_t log_ceiling_in_series(std::uint32_t base, std::uint32_t exponent, std::uint64_t sum)
+std::uint64_t ceiling_bound(std::uint32_t base, std::uint32_t multiplier, std::uint32_t sum)
 {
+    if (multiplier < 2U)
+    {
+	throw invalid_multiplier_error("Multiplier cannot be less than 2");
+    }
     std::uint64_t log = 0U;
     std::uint64_t current = base;
     for (std::uint64_t tally = 0U; tally < sum;)
     {
 	tally += current;
-	current *= exponent;
+	current *= multiplier;
 	++log;
     }
     return log;
--- src/math/summation.hpp
+++ src/math/summation.hpp
@@ -2,11 +2,20 @@
 #define TURBO_MATH_SUMMATION_HPP
 
 #include <cstdint>
+#include <stdexcept>
+#include <string>
 
 namespace turbo {
 namespace math {
 
-std::uint64_t log_ceiling_in_series(std::uint32_t base, std::uint32_t exponent, std::uint64_t sum);
+class invalid_multiplier_error : std::invalid_argument
+{
+public:
+    invalid_multiplier_error(const char* what) : invalid_argument(what) { }
+    invalid_multiplier_error(const std::string& what) : invalid_argument(what) { }
+};
+
+std::uint64_t ceiling_bound(std::uint32_t base, std::uint32_t multiplier, std::uint32_t sum);
 
 
 } // namespace math
