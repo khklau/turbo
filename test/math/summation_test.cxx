82a256f8eede4af6688a3d2cb1aad9c53debcbac
Kean H Lau <khklau@users.noreply.github.com>
2016-09-05 09:41:56 +1000

    adding more test cases

--- test/math/summation_test.cxx
+++ test/math/summation_test.cxx
@@ -3,12 +3,19 @@
 
 namespace tma = turbo::math;
 
-TEST(summation_test, basic_log_ceiling_in_series)
+TEST(summation_test, basic_ceiling_bound)
 {
-    EXPECT_EQ(0U, tma::log_ceiling_in_series(4U, 2U, 0U)) << "Wrong logarithm for sum of 0";
-    EXPECT_EQ(1U, tma::log_ceiling_in_series(4U, 2U, 2U)) << "Wrong logarithm for sum that is less than base";
-    EXPECT_EQ(1U, tma::log_ceiling_in_series(4U, 2U, 4U)) << "Wrong logarithm for sum equal to base";
-    EXPECT_EQ(2U, tma::log_ceiling_in_series(4U, 2U, 5U)) << "Wrong logarithm for sum just large than base";
-    EXPECT_EQ(2U, tma::log_ceiling_in_series(4U, 2U, 12U)) << "Wrong logarithm for sum of 2 exponents";
-    EXPECT_EQ(3U, tma::log_ceiling_in_series(4U, 2U, 13U)) << "Wrong logarithm for sum just larger than base squared";
+    EXPECT_EQ(1U, tma::ceiling_bound(4U, 2U, 2U)) << "Wrong logarithm for sum that is less than base";
+    EXPECT_EQ(1U, tma::ceiling_bound(4U, 2U, 4U)) << "Wrong logarithm for sum equal to base";
+    EXPECT_EQ(2U, tma::ceiling_bound(4U, 2U, 5U)) << "Wrong logarithm for sum just large than base";
+    EXPECT_EQ(2U, tma::ceiling_bound(4U, 2U, 12U)) << "Wrong logarithm for sum of 2 exponents";
+    EXPECT_EQ(3U, tma::ceiling_bound(4U, 2U, 13U)) << "Wrong logarithm for sum just larger than base squared";
+    EXPECT_EQ(3U, tma::ceiling_bound(4U, 2U, 28U)) << "Wrong logarithm for sum of 3 exponents";
+}
+
+TEST(summation_test, invalid_ceiling_bound)
+{
+    EXPECT_EQ(0U, tma::ceiling_bound(4U, 2U, 0U)) << "Wrong logarithm for sum of 0";
+    ASSERT_THROW(tma::ceiling_bound(4U, 0U, 12U), tma::invalid_multiplier_error) << "Exception not thrown for invalid multiplier";
+    ASSERT_THROW(tma::ceiling_bound(4U, 1U, 6U), tma::invalid_multiplier_error) << "Exception not thrown for invalid multiplier";
 }
