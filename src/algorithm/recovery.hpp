8f3edc6090ed799750928ca968d943ef0f45ae0e
Kean H Lau <khklau@users.noreply.github.com>
2016-09-23 19:30:22 +1000

    adding try_and_ensure template function

--- src/algorithm/recovery.hpp
+++ src/algorithm/recovery.hpp
@@ -16,6 +16,9 @@ enum class try_state
 
 TURBO_SYMBOL_DECL void retry_with_random_backoff(const std::function<try_state ()>& func, uint64_t max_backoff = 32U);
 
+template <class try_clause_t, class ensure_clause_t>
+TURBO_SYMBOL_DECL inline void try_and_ensure(const try_clause_t& try_clause, const ensure_clause_t& ensure_clause);
+
 } // namespace recovery
 } // namespace algorithm
 } // namespace turbo
---
+++ src/algorithm/recovery.hxx
@@ -0,0 +1,33 @@
+#ifndef TURBO_ALGORITHM_RECOVERY_HXX
+#define TURBO_ALGORITHM_RECOVERY_HXX
+
+#include <turbo/algorithm/recovery.hpp>
+
+namespace turbo {
+namespace algorithm {
+namespace recovery {
+
+template <class try_clause_t, class ensure_clause_t>
+void try_and_ensure(const try_clause_t& try_clause, const ensure_clause_t& ensure_clause)
+{
+    struct impl_
+    {
+	impl_(const ensure_clause_t& clause)
+	    :
+		clause_(clause)
+	{ }
+	~impl_()
+	{
+	    clause_();
+	}
+	const ensure_clause_t& clause_;
+    };
+    impl_ tmp(ensure_clause);
+    try_clause();
+}
+
+} // namespace recovery
+} // namespace algorithm
+} // namespace turbo
+
+#endif
