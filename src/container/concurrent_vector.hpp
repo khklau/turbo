7c5332dccf78c8f666c78955b19f3328ea7d5e44
Kean H Lau <khklau@users.noreply.github.com>
2016-09-27 23:31:21 +1000

    * the node guard is now versioned * added the descriptor definition, the pool of descriptors and current descriptor reference * added the complete_write member function * the constructor no longer pre-fills the first segment

--- src/container/concurrent_vector.hpp
+++ src/container/concurrent_vector.hpp
@@ -33,9 +33,10 @@ template <class value_t, template <class type_t> class allocator_t = std::alloca
 class TURBO_SYMBOL_DECL concurrent_vector
 {
 public:
+    typedef std::uint16_t throughput_type;
     typedef std::uint32_t capacity_type;
-    typedef std::pair<capacity_type, capacity_type> subscript_type;
     concurrent_vector(std::uint8_t initial_capacity_exponent, std::uint8_t max_capacity_exponent);
+    concurrent_vector(std::uint8_t initial_capacity_exponent, std::uint8_t max_capacity_exponent, throughput_type max_concurrent_writes);
     ~concurrent_vector();
     value_t& operator[](capacity_type index);
     const value_t& operator[](capacity_type index) const;
@@ -44,20 +45,45 @@ public:
 private:
     struct alignas(alignof(value_t)) node
     {
-	enum class status : uint8_t
+	enum class status : uint16_t
 	{
 	    ready,
 	    updating
 	};
+	struct versioned_guard
+	{
+	    status guard_status;
+	    std::uint16_t guard_version;
+	};
 	value_t value;
-	std::atomic<status> guard;
+	std::atomic<versioned_guard> guard;
     };
+    struct descriptor
+    {
+	descriptor();
+	descriptor(std::size_t size_, std::size_t capacity_, bool pending_, std::uint16_t version_, value_t&& value_, node* location_);
+	std::size_t size;
+	std::size_t capacity;
+	std::atomic<bool> has_pending_write;
+	std::uint16_t expected_version;
+	value_t new_value;
+	node* location;
+    };
+    struct descriptor_reference
+    {
+	throughput_type index;
+	std::uint16_t version;
+    };
+    typedef std::pair<capacity_type, capacity_type> subscript_type;
     static const std::uint8_t capacity_base_ = 2U;
+    void complete_write(descriptor& operation);
     subscript_type find_subscript(capacity_type index) const;
     void range_check(capacity_type index) const;
     const std::uint8_t initial_exponent_;
     const std::uint8_t max_exponent_;
     std::unique_ptr<std::atomic<node*>[]> segments_;
+    std::unique_ptr<descriptor[]> descriptors_;
+    std::atomic<descriptor_reference> current_descriptor_;
 };
 
 } // namespace container
--- src/container/concurrent_vector.hxx
+++ src/container/concurrent_vector.hxx
@@ -3,6 +3,7 @@
 
 #include <turbo/container/concurrent_vector.hpp>
 #include <limits>
+#include <turbo/algorithm/recovery.hpp>
 #include <turbo/math/power.hpp>
 #include <turbo/math/summation.hpp>
 #include <turbo/toolset/extension.hpp>
@@ -12,13 +13,54 @@ namespace turbo {
 namespace container {
 
 template <class value_t, template <class type_t> class allocator_t>
+concurrent_vector<value_t, allocator_t>::descriptor::descriptor()
+    :
+	size(0U),
+	capacity(0U),
+	expected_version(0U),
+	new_value(),
+	location(nullptr)
+{
+    has_pending_write.store(false, std::memory_order_release);
+}
+
+template <class value_t, template <class type_t> class allocator_t>
+concurrent_vector<value_t, allocator_t>::descriptor::descriptor(
+	std::size_t size_,
+	std::size_t capacity_,
+	bool pending_,
+	std::uint16_t version_,
+	value_t&& value_,
+	node* location_)
+    :
+	size(size_),
+	capacity(capacity_),
+	expected_version(version_),
+	new_value(std::move(value_)),
+	location(location_)
+{
+    has_pending_write.store(pending_, std::memory_order_release);
+}
+
+template <class value_t, template <class type_t> class allocator_t>
 concurrent_vector<value_t, allocator_t>::concurrent_vector(
 	std::uint8_t initial_capacity_exponent,
 	std::uint8_t max_capacity_exponent)
     :
+	concurrent_vector(initial_capacity_exponent, max_capacity_exponent, 0U)
+{ }
+
+template <class value_t, template <class type_t> class allocator_t>
+concurrent_vector<value_t, allocator_t>::concurrent_vector(
+	std::uint8_t initial_capacity_exponent,
+	std::uint8_t max_capacity_exponent,
+	std::uint16_t max_concurrent_writes)
+    :
 	initial_exponent_(initial_capacity_exponent),
 	max_exponent_(max_capacity_exponent),
-	segments_()
+	segments_(),
+	descriptors_(),
+	current_descriptor_({ 0U, 0U })
 {
     if (std::numeric_limits<capacity_type>::digits < max_exponent_)
     {
@@ -31,24 +73,25 @@ concurrent_vector<value_t, allocator_t>::concurrent_vector(
     else
     {
 	segments_.reset(new std::atomic<node*>[max_exponent_ - initial_exponent_]);
-	std::size_t first_size = turbo::math::pow(capacity_base_, initial_exponent_);
-	segments_[0] = new node[first_size];
-	for (std::size_t iter = 0U; iter < first_size; ++iter)
-	{
-	    segments_[0][iter].guard = node::status::ready;
-	}
-	std::size_t range = max_exponent_ - initial_exponent_;
+	const std::size_t first_segment_capacity = turbo::math::pow(capacity_base_, initial_exponent_);
+	segments_[0] = new node[first_segment_capacity];
+	const std::size_t range = max_exponent_ - initial_exponent_;
 	for (std::size_t iter = 1U; iter < range; ++iter)
 	{
 	    segments_[iter] = nullptr;
 	}
+	const std::size_t descriptor_count = (max_concurrent_writes == 0U ) ?
+		std::numeric_limits<std::uint16_t>::max() :
+		max_concurrent_writes;
+	descriptors_.reset(new descriptor[descriptor_count]);
+	new (&(descriptors_[0])) descriptor(0U, first_segment_capacity, false, 0U, value_t(), nullptr);
     }
 }
 
 template <class value_t, template <class type_t> class allocator_t>
 concurrent_vector<value_t, allocator_t>::~concurrent_vector()
 {
-    std::size_t range = max_exponent_ - initial_exponent_;
+    const std::size_t range = max_exponent_ - initial_exponent_;
     for (std::size_t iter = 0U; iter < range; ++iter)
     {
 	node* tmp = segments_[iter].load(std::memory_order_acquire);
@@ -118,6 +161,33 @@ void concurrent_vector<value_t, allocator_t>::range_check(capacity_type index) c
     else if (segments_[subscript.first] == nullptr)
     {
 	throw std::out_of_range("Requested element is not in range");
+    }
+}
+
+template <class value_t, template <class type_t> class allocator_t>
+void concurrent_vector<value_t, allocator_t>::complete_write(descriptor& operation)
+{
+    bool is_pending = operation.has_pending_write.load(std::memory_order_acquire);
+    if (is_pending)
+    {
+	if (operation.location != nullptr)
+	{
+	    typename node::versioned_guard guard = operation.location->guard.load(std::memory_order_acquire);
+	    if (guard.guard_status == node::status::ready && guard.guard_version == operation.expected_version)
+	    {
+		operation.location.guard.store({ node::status::updating, guard.guard_version}, std::memory_order_release);
+		turbo::algorithm::recovery::try_and_ensure(
+		[&] ()
+		{
+		    operation.location.value = operation.new_value;
+		},
+		[&] ()
+		{
+		    operation.location.guard.store({ node::status::ready, guard.guard_version + 1}, std::memory_order_release);
+		});
+	    }
+	}
+	operation.has_pending_write.store(false, std::memory_order_release);
     }
 }
 
