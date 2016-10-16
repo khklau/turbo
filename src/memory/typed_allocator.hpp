6e63674856baebe789a61f05c1cae7af6445ed7c
Kean H Lau <khklau@users.noreply.github.com>
2016-10-16 22:03:27 +1100

    removed default arguments and instead provided overloads

--- src/memory/typed_allocator.hpp
+++ src/memory/typed_allocator.hpp
@@ -15,9 +15,17 @@ class typed_allocator
 public:
     typedef std::size_t size_type;
     template <class value_t>
-    inline typename value_t* allocate(size_type quantity = 1U, const value_t* hint = nullptr) { return std::allocator<value_t>::allocate(quantity, hint); }
+    inline typename value_t* allocate() { return std::allocator<value_t>::allocate(1U); }
     template <class value_t>
-    inline void dellocate(value_t* pointer, size_type quantity = 1U) { std::allocator<value_t>::deallocate(pointer, quantity); }
+    inline typename value_t* allocate(size_type quantity) { return std::allocator<value_t>::allocate(quantity); }
+    template <class value_t>
+    inline typename value_t* allocate(const value_t* hint) { return std::allocator<value_t>::allocate(1U, hint); }
+    template <class value_t>
+    inline typename value_t* allocate(size_type quantity, const value_t* hint) { return std::allocator<value_t>::allocate(quantity, hint); }
+    template <class value_t>
+    inline void dellocate(value_t* pointer) { std::allocator<value_t>::deallocate(pointer, 1U); }
+    template <class value_t>
+    inline void dellocate(value_t* pointer, size_type quantity) { std::allocator<value_t>::deallocate(pointer, quantity); }
 };
 
 } // namespace memory
