2bb51dc41708f4de0b8993dbcda33975a062e233
Kean H Lau <khklau@users.noreply.github.com>
2016-10-31 19:03:06 +1100

    removing the redundant block_pool class which has been succeeded by pool

--- src/memory/pool.hpp
+++ src/memory/pool.hpp
@@ -29,26 +29,6 @@ enum class make_result
     pool_full
 };
 
-template <std::size_t block_size_c, template <class type_t> class allocator_t = std::allocator>
-class block_pool
-{
-public:
-    block_pool(capacity_type capacity);
-    std::size_t get_byte_size() const;
-    const void* get_base_address() const;
-    std::pair<make_result, void*> allocate();
-    void free(void* pointer);
-    template <class value_t, class... args_t>
-    std::pair<make_result, pool_unique_ptr<value_t>> make_unique(args_t&&... args);
-    template <class value_t, class... args_t>
-    std::pair<make_result, std::shared_ptr<value_t>> make_shared(args_t&&... args);
-private:
-    typedef typename std::aligned_storage<block_size_c>::type block_type;
-    typedef turbo::container::mpmc_ring_queue<capacity_type, allocator_t> free_list_type;
-    free_list_type free_list_;
-    typename std::vector<block_type, allocator_t<block_type>> block_list_;
-};
-
 struct block_config
 {
     block_config();
