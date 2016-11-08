342962644212b66e154169a8a1c8bc50524919c3
Kean H Lau <khklau@users.noreply.github.com>
2016-11-08 23:26:00 +1100

    typename declaration not needed

--- src/memory/typed_allocator.hpp
+++ src/memory/typed_allocator.hpp
@@ -15,13 +15,13 @@ class typed_allocator
 public:
     typedef std::size_t size_type;
     template <class value_t>
-    inline typename value_t* allocate() { return std::allocator<value_t>::allocate(1U); }
+    inline value_t* allocate() { return std::allocator<value_t>::allocate(1U); }
     template <class value_t>
-    inline typename value_t* allocate(size_type quantity) { return std::allocator<value_t>::allocate(quantity); }
+    inline value_t* allocate(size_type quantity) { return std::allocator<value_t>::allocate(quantity); }
     template <class value_t>
-    inline typename value_t* allocate(const value_t* hint) { return std::allocator<value_t>::allocate(1U, hint); }
+    inline value_t* allocate(const value_t* hint) { return std::allocator<value_t>::allocate(1U, hint); }
     template <class value_t>
-    inline typename value_t* allocate(size_type quantity, const value_t* hint) { return std::allocator<value_t>::allocate(quantity, hint); }
+    inline value_t* allocate(size_type quantity, const value_t* hint) { return std::allocator<value_t>::allocate(quantity, hint); }
     template <class value_t>
     inline void dellocate(value_t* pointer) { std::allocator<value_t>::deallocate(pointer, 1U); }
     template <class value_t>
