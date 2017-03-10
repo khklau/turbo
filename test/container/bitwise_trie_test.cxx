ea16af46d2494134ef6dde48f9d744d89c4e8b4e
Kean H Lau <khklau@users.noreply.github.com>
2017-03-10 22:59:03 +1100

    added functional and performance tests for find

--- test/container/bitwise_trie_test.cxx
+++ test/container/bitwise_trie_test.cxx
@@ -62,6 +62,9 @@ TEST(bitwise_trie_test, empty_trie)
     auto iter2 = map1.crbegin();
     ++iter2;
     EXPECT_EQ(map1.crend(), iter2) << "Finding the successor of the begin iterator from an empty trie succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(0U)) << "Find on empty trie succedded";
+    EXPECT_EQ(map1.cend(), map1.find(128U)) << "Find on empty trie succedded";
+    EXPECT_EQ(map1.cend(), map1.find(255U)) << "Find on empty trie succedded";
 }
 
 TEST(bitwise_trie_test, emplace_invalid)
@@ -91,8 +94,6 @@ TEST(bitwise_trie_test, emplace_basic)
     EXPECT_NE(map1.end(), std::get<0>(result1)) << "Emplace failed";
     EXPECT_EQ(std::string("bar"), *std::get<0>(result1)) << "Emplace failed";
     EXPECT_EQ(1U, map1.size()) << "Size of trie after 1 emplace is not 1";
-    EXPECT_NE(map1.cend(), map1.find(64U)) << "Could not find just emplaced key & value";
-    EXPECT_EQ(std::string("bar"), *map1.find(64U)) << "Could not find just emplaced key & value";
     EXPECT_EQ(std::string("bar"), *map1.cbegin()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(64U, map1.cbegin().get_key()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(std::string("bar"), *map1.crbegin()) << "Just emplaced key & value is not ordered";
@@ -102,8 +103,6 @@ TEST(bitwise_trie_test, emplace_basic)
     EXPECT_NE(map1.end(), std::get<0>(result2)) << "Emplace failed";
     EXPECT_EQ(std::string("foo"), *std::get<0>(result2)) << "Emplace failed";
     EXPECT_EQ(2U, map1.size()) << "Size of trie after 2 emplace is not 2";
-    EXPECT_NE(map1.cend(), map1.find(32U)) << "Could not find just emplaced key & value";
-    EXPECT_EQ(std::string("foo"), *map1.find(32U)) << "Could not find just emplaced key & value";
     EXPECT_EQ(std::string("foo"), *map1.cbegin()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(32U, map1.cbegin().get_key()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(std::string("bar"), *map1.crbegin()) << "Just emplaced key & value is not ordered";
@@ -113,24 +112,10 @@ TEST(bitwise_trie_test, emplace_basic)
     EXPECT_NE(map1.end(), std::get<0>(result3)) << "Emplace failed";
     EXPECT_EQ(std::string("blah"), *std::get<0>(result3)) << "Emplace failed";
     EXPECT_EQ(3U, map1.size()) << "Size of trie after 3 emplace is not 3";
-    EXPECT_NE(map1.cend(), map1.find(128U)) << "Could not find just emplaced key & value";
-    EXPECT_EQ(std::string("blah"), *map1.find(128U)) << "Could not find just emplaced key & value";
     EXPECT_EQ(std::string("foo"), *map1.cbegin()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(32U, map1.cbegin().get_key()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(std::string("blah"), *map1.crbegin()) << "Just emplaced key & value is not ordered";
     EXPECT_EQ(128U, map1.crbegin().get_key()) << "Just emplaced key & value is not ordered";
-    auto iter1 = map1.cbegin();
-    EXPECT_EQ(std::string("foo"), *iter1) << "Just emplaced key & value is not ordered";
-    ++iter1;
-    EXPECT_EQ(std::string("bar"), *iter1) << "Just emplaced key & value is not ordered";
-    ++iter1;
-    EXPECT_EQ(std::string("blah"), *iter1) << "Just emplaced key & value is not ordered";
-    auto iter2 = map1.crbegin();
-    EXPECT_EQ(std::string("blah"), *iter2) << "Just emplaced key & value is not ordered";
-    ++iter2;
-    EXPECT_EQ(std::string("bar"), *iter2) << "Just emplaced key & value is not ordered";
-    ++iter2;
-    EXPECT_EQ(std::string("foo"), *iter2) << "Just emplaced key & value is not ordered";
 }
 
 TEST(bitwise_trie_test, begin_basic)
@@ -153,6 +138,63 @@ TEST(bitwise_trie_test, begin_basic)
     auto iter2b = map2.crbegin();
     EXPECT_NE(map2.crend(), iter2b) << "Could not first value";
     EXPECT_EQ(std::string("foo"), *iter2b) << "Could not first value";
+}
+
+TEST(bitwise_trie_test, find_invalid)
+{
+    typedef tco::bitwise_trie<std::uint8_t, std::string, tme::pool> string_map;
+    tme::pool allocator1(8U, { {string_map::node_sizes[0], 8U}, {string_map::node_sizes[1], 8U} });
+    string_map map1(allocator1);
+    map1.emplace(32U, "foo");
+    map1.emplace(64U, "bar");
+    map1.emplace(128U, "blah");
+    EXPECT_EQ(map1.cend(), map1.find(0U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(31U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(33U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(63U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(65U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(127U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(129U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map1.cend(), map1.find(255U)) << "Find on non-existent key succeeded";
+    string_map map2(allocator1);
+    map2.emplace(126U, "foo");
+    map2.emplace(127U, "bar");
+    map2.emplace(128U, "blah");
+    EXPECT_EQ(map2.cend(), map2.find(0U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map2.cend(), map2.find(125U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map2.cend(), map2.find(129U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map2.cend(), map2.find(255U)) << "Find on non-existent key succeeded";
+    string_map map3(allocator1);
+    map3.emplace(0U, "foo");
+    map3.emplace(255U, "bar");
+    EXPECT_EQ(map3.cend(), map3.find(1U)) << "Find on non-existent key succeeded";
+    EXPECT_EQ(map3.cend(), map3.find(254U)) << "Find on non-existent key succeeded";
+}
+
+TEST(bitwise_trie_test, find_basic)
+{
+    typedef tco::bitwise_trie<std::uint8_t, std::string, tme::pool> string_map;
+    tme::pool allocator1(8U, { {string_map::node_sizes[0], 8U}, {string_map::node_sizes[1], 8U} });
+    string_map map1(allocator1);
+    map1.emplace(64U, "bar");
+    map1.emplace(32U, "foo");
+    map1.emplace(128U, "blah");
+    EXPECT_NE(map1.cend(), map1.find(128U)) << "Could not find just emplaced key & value";
+    EXPECT_EQ(std::string("blah"), *map1.find(128U)) << "Could not find just emplaced key & value";
+    EXPECT_NE(map1.cend(), map1.find(64U)) << "Could not find just emplaced key & value";
+    EXPECT_EQ(std::string("bar"), *map1.find(64U)) << "Could not find just emplaced key & value";
+    EXPECT_NE(map1.cend(), map1.find(32U)) << "Could not find just emplaced key & value";
+    EXPECT_EQ(std::string("foo"), *map1.find(32U)) << "Could not find just emplaced key & value";
+    string_map map2(allocator1);
+    map2.emplace(126U, "foo");
+    map2.emplace(127U, "bar");
+    map2.emplace(128U, "blah");
+    EXPECT_NE(map1.cend(), map2.find(126U)) << "Could not find just emplaced key & value";
+    EXPECT_EQ(std::string("foo"), *map2.find(126U)) << "Could not find just emplaced key & value";
+    EXPECT_NE(map1.cend(), map2.find(127U)) << "Could not find just emplaced key & value";
+    EXPECT_EQ(std::string("bar"), *map2.find(127U)) << "Could not find just emplaced key & value";
+    EXPECT_NE(map1.cend(), map2.find(128U)) << "Could not find just emplaced key & value";
+    EXPECT_EQ(std::string("blah"), *map2.find(128U)) << "Could not find just emplaced key & value";
 }
 
 TEST(bitwise_trie_test, forward_successor_invalid)
@@ -403,22 +445,22 @@ TEST(bitwise_trie_test, reverse_predecessor_basic)
     EXPECT_EQ(std::string("blah"), *iter2) << "Could not find predecessor";
 }
 
-class bitwise_trie_perf_test : public ::testing::Test
+class bitwise_trie_emplace_perf_test : public ::testing::Test
 {
 public:
-    typedef tco::bitwise_trie<std::uint64_t, std::uint64_t, tme::pool> uint_map;
-    bitwise_trie_perf_test()
+    typedef tco::bitwise_trie<std::uint64_t, std::uint64_t, tme::pool> uint_trie;
+    bitwise_trie_emplace_perf_test()
 	:
-	    allocator1(8U, { {uint_map::node_sizes[0], 1U << 15U}, {uint_map::node_sizes[1], (1U << 16U) - 1U} })
+	    allocator1(8U, { {uint_trie::node_sizes[0], 1U << 15U}, {uint_trie::node_sizes[1], (1U << 16U) - 1U} })
     { }
 protected:
     tme::pool allocator1;
 };
 
-TEST_F(bitwise_trie_perf_test, perf_test_trie_emplace)
+TEST_F(bitwise_trie_emplace_perf_test, perf_test_trie_emplace)
 {
-    uint_map map1(allocator1);
-    std::mt19937 device(std::chrono::system_clock::now().time_since_epoch().count());
+    uint_trie map1(allocator1);
+    std::random_device device;
     for (std::uint64_t counter = 0U; counter <= std::numeric_limits<std::uint16_t>::max(); ++counter)
     {
 	std::uint64_t value = device() >> 16U;
@@ -426,13 +468,57 @@ TEST_F(bitwise_trie_perf_test, perf_test_trie_emplace)
     }
 }
 
-TEST_F(bitwise_trie_perf_test, perf_test_map_emplace)
+TEST_F(bitwise_trie_emplace_perf_test, perf_test_map_emplace)
 {
     std::map<std::uint64_t, std::uint64_t> map1;
-    std::mt19937 device(std::chrono::system_clock::now().time_since_epoch().count());
+    std::random_device device;
     for (std::uint64_t counter = 0U; counter <= std::numeric_limits<std::uint16_t>::max(); ++counter)
     {
 	std::uint64_t value = device() >> 16U;
 	map1.emplace(value, value);
+    }
+}
+
+class bitwise_trie_find_perf_test : public ::testing::Test
+{
+public:
+    typedef tco::bitwise_trie<std::uint64_t, std::uint64_t, tme::pool> uint_trie;
+    typedef std::map<std::uint64_t, std::uint64_t> uint_map;
+    bitwise_trie_find_perf_test()
+	:
+	    allocator(8U, { {uint_trie::node_sizes[0], 1U << 15U}, {uint_trie::node_sizes[1], (1U << 16U) - 1U} }),
+	    trie(allocator),
+	    map(),
+	    values(1U << 15U)
+    {
+	std::random_device device;
+	for (std::uint64_t counter = 0U; counter <= std::numeric_limits<std::uint16_t>::max(); ++counter)
+	{
+	    std::uint64_t value = device() >> 16U;
+	    values.emplace_back(value);
+	    trie.emplace(value, value);
+	    map.emplace(value, value);
+	}
+    }
+protected:
+    tme::pool allocator;
+    uint_trie trie;
+    uint_map map;
+    std::vector<std::uint64_t> values;
+};
+
+TEST_F(bitwise_trie_find_perf_test, perf_test_trie_find)
+{
+    for (auto value: values)
+    {
+	trie.find(value);
+    }
+}
+
+TEST_F(bitwise_trie_find_perf_test, perf_test_map_find)
+{
+    for (auto value: values)
+    {
+	map.find(value);
     }
 }
